6553a753c9b5c52fd4126d07e9d806decc6d40a4
Timotej Králik <timotej.kralik@muziker.com>
2024-03-04 17:23:15 +0000

    Changing IJEPA to MS COCO

--- ijepa/src/datasets/imagenet1k.py
+++ ijepa/src/datasets/imagenet1k.py
@@ -63,6 +63,99 @@ def make_imagenet1k(
     return dataset, data_loader, dist_sampler
 
 
+def make_COCO(
+  transform,
+  batch_size,
+  collator=None,
+  pin_mem=True,
+  num_workers=8,
+  world_size=1,
+  rank=0,
+  root_path=None,
+  image_folder=None,
+  training=True,
+  copy_data=False,
+  drop_last=True,
+  subset_file=None
+):
+  
+  dataset = CocoDetection('/content/Dataset_dp/train2017', '/content/Dataset_dp/annotations/person_keypoints_train2017.json', transform=transform)
+  logger.info('ImageNet dataset created')
+
+
+  data_loader = torch.utils.data.DataLoader(
+        dataset,
+        batch_size=batch_size,
+        num_workers=num_workers,
+        pin_memory=pin_mem,
+        shuffle=False,
+        # sampler=sampler
+    )
+      
+  logger.info('ImageNet unsupervised data loader created')
+
+  return dataset, data_loader, None
+
+
+class CocoDetection(torch.utils.data.Dataset):
+    """`MS Coco Detection <http://mscoco.org/dataset/#detections-challenge2016>`_ Dataset.
+
+    Args:
+        root (string): Root directory where images are downloaded to.
+        annFile (string): Path to json annotation file.
+        transform (callable, optional): A function/transform that  takes in an PIL image
+            and returns a transformed version. E.g, ``transforms.ToTensor``
+        target_transform (callable, optional): A function/transform that takes in the
+            target and transforms it.
+    """
+
+    def __init__(self, root, annFile, transform=None, target_transform=None):
+        from pycocotools.coco import COCO
+        self.root = root
+        self.coco = COCO(annFile)
+        self.ids = list(self.coco.imgs.keys())
+        self.transform = transform
+        self.target_transform = target_transform
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: Tuple (image, target). target is the object returned by ``coco.loadAnns``.
+        """
+        coco = self.coco
+        img_id = self.ids[index]
+        ann_ids = coco.getAnnIds(imgIds=img_id)
+        target = coco.loadAnns(ann_ids)
+
+        path = coco.loadImgs(img_id)[0]['file_name']
+
+        img = Image.open(os.path.join(self.root, path)).convert('RGB')
+        if self.transform is not None:
+            img = self.transform(img)
+
+        if self.target_transform is not None:
+            target = self.target_transform(target)
+
+        return img
+
+    def __len__(self):
+        return len(self.ids)
+
+    def __repr__(self):
+        fmt_str = 'Dataset ' + self.__class__.__name__ + '\n'
+        fmt_str += '    Number of datapoints: {}\n'.format(self.__len__())
+        fmt_str += '    Root Location: {}\n'.format(self.root)
+        tmp = '    Transforms (if any): '
+        fmt_str += '{0}{1}\n'.format(tmp, self.transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        tmp = '    Target Transforms (if any): '
+        fmt_str += '{0}{1}'.format(tmp, self.target_transform.__repr__().replace('\n', '\n' + ' ' * len(tmp)))
+        return fmt_str
+
+
+
 class ImageNet(torchvision.datasets.ImageFolder):
 
     def __init__(
